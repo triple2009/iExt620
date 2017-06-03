3f021d21f366a16b753101a506f65b8808a5e0af
yuyu yang <yuyuyang@yuyudeMacBook-Pro.local>
2017-06-03 15:21:15 +0800

    init

--- packages/local/iext-classic/examples/app/meta/User.js
+++ packages/local/iext-classic/examples/app/meta/User.js
@@ -27,32 +27,6 @@
         ixName: 'statusName',
         ixTitle: '状态',
         ixSubType: iExt.meta.ixtype.String.STRING
-    }],
-
-    ixActions: {
-        test: {
-            type: 'view',
-            ixName: '测试'
-        },
-        _add: {
-            ixName: '新增'
-        },
-        active: {
-            type: 'api',
-            ixName: '有效'
-        },
-        inactive: {
-            type: 'api',
-            ixName: '无效'
-        },
-        rest: {
-            type: 'api',
-            ixName: '重置口令'
-        },
-        changePwd: {
-            type: 'view',
-            ixName: '变更口令'
-        }
-    }
+    }]
 
 });
